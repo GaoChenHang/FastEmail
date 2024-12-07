3a2a49e6b1a0c5d12f81bf0b83c04e702ca6f1cd
GaoChenHang <3992412947@qq.com>
2024-12-07 18:54:27 +0800

    初始化项目

---
+++ FastEmail_BE/src/main/java/github/gaochenhang/fastemail_be/FastEmailBeApplication.java
@@ -0,0 +1,13 @@
+package github.gaochenhang.fastemail_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastEmailBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastEmailBeApplication.class, args);
+    }
+
+}
---
+++ FastEmail_BE/src/test/java/github/gaochenhang/fastemail_be/FastEmailBeApplicationTests.java
@@ -0,0 +1,13 @@
+package github.gaochenhang.fastemail_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastEmailBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
